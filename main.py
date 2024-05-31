2e87993ae8589d7c8c81977b7f7aff3dc8105a16
Denis Prelog <denis.prelog1@gmail.com>
2024-05-31 08:02:45 +0200

    Add .env variables

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
+import os
+from dotenv import load_dotenv
 from telethon import TelegramClient, events
 from Parser import Parser
 from Draw import Draw
 
-api_id = 24023329
-api_hash = "331c5c7e70b57043e03849acbbc1202a"
-user_input_channel = "https://t.me/+diPUlUMl9ns4ODZk"
+load_dotenv()
+
+api_id = os.getenv("API_ID", "")
+api_hash = os.getenv("API_HASH", "")
+user_input_channel = os.getenv("USER_INPUT_CHANEL", "")
 
 client = TelegramClient("session_name", api_id, api_hash)
 parser = Parser()
 draw = Draw()
 
 
-"""
 @client.on(events.NewMessage(chats=user_input_channel))
 async def newMessageListener(event):
     data = parser.parse_data(event.message.message)
+    draw.draw(data)
 
     # create image
     # post to instagram
@@ -24,22 +28,3 @@ async def newMessageListener(event):
 
 with client:
     client.run_until_disconnected()
-"""
-
-
-tmp = [
-    "Daily income of S-Group for May 28 💸 ",
-    "",
-    "🔹Profit in S-Forex was +0.19%",
-    "",
-    "🔹Profit in S-Forex Pro was +0.50%",
-    "",
-    "🔹 Profit in S-Forex Optimal (I pool) was +0.53%",
-    "",
-    "🔹 Profit in S-Forex Optimal (II pool) was +0.39%",
-    "",
-    "🔹Profit in S-Forex Prime was +0.58%",
-]
-data = parser.parse_data(tmp, True)
-
-draw.draw(data)
